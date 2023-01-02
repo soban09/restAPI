c9d5dbe462ef602d15b68ab79a4a9ce7b6c9356b
Soban Farooq <sobanfarooq911@gmail.com>
2023-01-02 20:17:00 +0530

    update3

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-const app = express()
+const express = require("express");
+const bodyParser = require("body-parser");
+const https = require("https");
 const PORT = 3000
+const app = express();
 
-import routerRoutes from './routes/users.js'
+app.use(bodyParser.urlencoded({extended:true}));
 
-app.use(bodyParser.json())
-app.use('/users', routerRoutes)
+app.use(express.static("public"));
 
 app.get('/', (req, res) => {
     console.log('Hello') 
-    res.sendFile('./index.html')
+    res.sendFile(__dirname + '/index.html')
 })
 
 app.listen(PORT, () => console.log(`Server is running on port : ${PORT}`))
